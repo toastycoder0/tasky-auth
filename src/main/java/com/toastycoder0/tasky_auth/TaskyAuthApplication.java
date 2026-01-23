8b103cb4717efe0371e3e26d0a34e78852ee3f89
toastycoder0 <re-jos2002@proton.me>
2026-01-22 23:31:09 -0600

    project started

---
+++ src/main/java/com/toastycoder0/tasky_auth/TaskyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.toastycoder0.tasky_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskyAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskyAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toastycoder0/tasky_auth/TaskyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toastycoder0.tasky_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskyAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
