7aecff647a17d6b0da12a2d9c94b254645936fe5
toastycoder0 <re-jos2002@proton.me>
2026-01-27 23:39:47 -0600

    UserController agregado

---
+++ src/main/java/com/toastycoder0/tasky_auth/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.toastycoder0.tasky_auth.controllers;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.toastycoder0.tasky_auth.models.UserModel;
+import com.toastycoder0.tasky_auth.services.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+  @Autowired
+  private UserService userService;
+
+  @GetMapping
+  public List<UserModel> getUsers() {
+    return userService.getAllUsers();
+  }
+
+  @GetMapping("/{id}")
+  public UserModel getUserById(@PathVariable UUID id) {
+    return userService.getUserById(id);
+  }
+
+  @PostMapping
+  public UserModel createUser(@RequestBody UserModel user) {
+    return userService.saveUser(user);
+  }
+}
