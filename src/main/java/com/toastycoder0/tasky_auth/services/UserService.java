c615b4a0561c790492a237bd5fdaf838e7250de0
toastycoder0 <re-jos2002@proton.me>
2026-02-03 22:51:32 -0600

    implementacion de error personalizado de no encontrado

--- src/main/java/com/toastycoder0/tasky_auth/services/UserService.java
+++ src/main/java/com/toastycoder0/tasky_auth/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.toastycoder0.tasky_auth.exceptions.ResourceNotFoundException;
 import com.toastycoder0.tasky_auth.models.UserModel;
 import com.toastycoder0.tasky_auth.repositories.UserRespository;
 
@@ -19,7 +20,7 @@ public class UserService {
   }
 
   public UserModel getUserById(UUID id) {
-    return userRespository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    return userRespository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado"));
   }
 
   public UserModel saveUser(UserModel user) {
@@ -32,14 +33,14 @@ public class UserService {
       user.setPassword(userDetails.getPassword());
       user.setActive(userDetails.isActive());
       return userRespository.save(user);
-    }).orElseThrow(() -> new RuntimeException("User not found"));
+    }).orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado"));
   }
 
   public void deleteUser(UUID id) {
     if (userRespository.existsById(id)) {
       userRespository.deleteById(id);
     } else {
-      throw new RuntimeException("User not found");
+      throw new ResourceNotFoundException("Usuario no encontrado");
     }
   }
 }
