fb5397720ee377711d0e4105849efc849fba232a
toastycoder0 <re-jos2002@proton.me>
2026-02-03 22:27:14 -0600

    controladores de eliminar y actualizar agregados

--- src/main/java/com/toastycoder0/tasky_auth/controllers/UserController.java
+++ src/main/java/com/toastycoder0/tasky_auth/controllers/UserController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +35,17 @@ public class UserController {
   @PostMapping
   public UserModel createUser(@RequestBody UserModel user) {
     return userService.saveUser(user);
+  }
+
+  @PutMapping("/{id}")
+  public UserModel updateUser(@PathVariable UUID id, @RequestBody UserModel user) {
+    System.out.println(id + " " + user.getEmail());
+
+    return userService.updateUser(id, user);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteUser(@PathVariable UUID id) {
+    userService.deleteUser(id);
   }
 }
