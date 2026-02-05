15cce6c1e2d903a1d1111107ceeba61c9112526f
Neto020220 <em807064@gmail.com>
2026-02-04 22:52:40 -0600

    Consulta a la base de datos

--- src/main/java/com/toastycoder0/tasky_auth/controllers/StatusController.java
+++ src/main/java/com/toastycoder0/tasky_auth/controllers/StatusController.java
@@ -1,17 +1,23 @@
 package com.toastycoder0.tasky_auth.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.toastycoder0.tasky_auth.dtos.GenericResponse;
+import com.toastycoder0.tasky_auth.repositories.UserRespository;
 
 @RestController
 @RequestMapping("/status")
 public class StatusController {
+  @Autowired
+  private UserRespository userRespository;
+
   @GetMapping
   public ResponseEntity<GenericResponse> getStatus() {
+    userRespository.count();
     return ResponseEntity.ok(new GenericResponse("Ok"));
   }
 }
